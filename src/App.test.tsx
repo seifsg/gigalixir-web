66b5c4ac6f3d6869c8c7be5c3c1aeb59c64b3396
Jesse <jesse@gigalixir.com>
2019-06-28 17:34:10 -0700

    makes test work.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as axios from "axios";
+
+jest.mock("axios");
 
 it('renders without crashing', () => {
+  axios.get.mockImplementation((url: string) => {
+    if (url.includes("/frontend/api/csrf")) {
+      Promise.resolve({ data: "fake-csrf-token" })
+    }
+  });
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
