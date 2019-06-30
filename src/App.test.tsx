952ad1182a7f5e0f76cc67fe87fc8960b64897a4
Jesse <jesse@gigalixir.com>
2019-06-30 10:07:48 -0700

    spy on functions and clean up instead of mocking the entire import. not too sure about this. helped me improve the api to return just data instead of an AxiosResponse<any>

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as axios from "axios";
-
-jest.mock("axios");
+import axios from "axios";
 
 it('renders without crashing', () => {
-  axios.get.mockImplementation((url: string) => {
-    if (url.includes("/frontend/api/csrf")) {
-      Promise.resolve({ data: "fake-csrf-token" })
-    }
-  });
+  const csrfMock = jest.spyOn(axios, 'get')
+  csrfMock.mockReturnValueOnce(Promise.resolve({ data: {data: "fake-csrf-token" }}))
+  const createSessionMock = jest.spyOn(axios, 'get')
+  createSessionMock.mockReturnValueOnce(Promise.resolve({ data: {data: "fake-session" }}))
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+  expect(csrfMock).toHaveBeenCalled()
+  expect(createSessionMock).toHaveBeenCalled()
+  csrfMock.mockRestore()
+  createSessionMock.mockRestore()
 });
--- src/authProvider.ts
+++ src/authProvider.ts
@@ -5,13 +5,7 @@ let host = "http://localhost:4000"
 
 function get_csrf() {
     return axios.get(host + '/frontend/api/csrf', { withCredentials: true })
-        .then(function(response) {
-            // handle success
-            console.log(response);
-            let csrf_token = response.data.data;
-            console.log(csrf_token);
-            return csrf_token;
-        })
+        .then(response => response.data.data)
 }
 
 export default (auth_type: any, params: any) => {
@@ -19,15 +13,18 @@ export default (auth_type: any, params: any) => {
     if (auth_type === AUTH_LOGIN) {
         const { username, password } = params;
         return get_csrf()
-            .then(function(csrf_token) {
-                return axios.post(host + '/frontend/api/sessions', { session: { email: username, password: password } }, { headers: { "X-CSRF-Token": csrf_token }, withCredentials: true });
+            .then(function (csrf_token) {
+                return axios.post(host + '/frontend/api/sessions', { session: { email: username, password: password } }, { headers: { "X-CSRF-Token": csrf_token }, withCredentials: true })
+                    .then(response => response.data)
             })
     }
     // called when the user clicks on the logout button
     if (auth_type === AUTH_LOGOUT) {
         return get_csrf()
-            .then(function(csrf_token) {
-                return axios.delete(host + '/frontend/api/sessions', { headers: { "X-CSRF-Token": csrf_token }, withCredentials: true });
+            .then(function (csrf_token) {
+                return axios.delete(host + '/frontend/api/sessions', { headers: { "X-CSRF-Token": csrf_token }, withCredentials: true })
+                    .then(response => response.data)
+
             })
     }
     // called when the API returns an error
@@ -35,15 +32,19 @@ export default (auth_type: any, params: any) => {
         const { status } = params;
         if (status === 401 || status === 403) {
             return get_csrf()
-                .then(function(csrf_token) {
-                    return axios.delete(host + '/frontend/api/sessions', { headers: { "X-CSRF-Token": csrf_token }, withCredentials: true });
+                .then(function (csrf_token) {
+                    return axios.delete(host + '/frontend/api/sessions', { headers: { "X-CSRF-Token": csrf_token }, withCredentials: true })
+                        .then(response => response.data)
+
                 })
         }
         return Promise.resolve();
     }
     // called when the user navigates to a new location
     if (auth_type === AUTH_CHECK) {
-        return axios.get(host + '/frontend/api/sessions', { withCredentials: true });
+        return axios.get(host + '/frontend/api/sessions', { withCredentials: true })
+            .then(response => response.data)
+
     }
     return Promise.reject('Unknown method');
 };
