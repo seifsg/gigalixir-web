921ee0c2d17a93f02d5fd0d81089d986b7fb77c5
Jesse <jesse@gigalixir.com>
2019-07-05 23:49:52 -0700

    switch from textinput to selectinput. try and stop using any type a little.

--- src/apps.tsx
+++ src/apps.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { List, Datagrid, TextField, NumberField, Create, SimpleForm, TextInput } from 'react-admin';
+import { SelectInput, List, Datagrid, TextField, NumberField, Create, SimpleForm, TextInput } from 'react-admin';
 
-export const AppList = ( props: any ) => (
+// not gonna go thru and do a whole list of stuff from here
+// https://marmelab.com/react-admin/List.html#the-list-component
+type ListProps = any
+
+export const AppList: React.FunctionComponent<ListProps> = ( props ): React.ReactElement => (
     <List {...props}>
         <Datagrid rowClick="edit">
             <TextField source="id" />
@@ -14,14 +18,44 @@ export const AppList = ( props: any ) => (
     </List>
 );
 
+const regionChoices = (cloud: string): {defaultValue: string; choices: {id: string; name: string}[]} => {
+    if (cloud === "gcp") {
+        return {defaultValue: 'v2018-us-central1', choices: [
+            { id: 'v2018-us-central1', name: 'v2018-us-central1' },
+            { id: 'europe-west1', name: 'europe-west1' },
+        ]}
+    } else if (cloud === 'aws') {
+        return {defaultValue: 'us-east-1', choices: [
+            { id: 'us-east-1', name: 'us-east-1' },
+            { id: 'us-west-2', name: 'us-west-2' },
+        ]}
+    } else {
+        return {defaultValue: '', choices: []};
+    }
+}
 
-export const AppCreate = ( props: any ) => (
-    <Create {...props}>
-        <SimpleForm>
-            <TextInput source="name" />
-            <TextInput source="cloud" />
-            <TextInput source="region" />
-        </SimpleForm>
-    </Create>
-);
+interface CreateProps {
+    title: string;
+    actions: React.ReactElement;
+    aside: React.ReactElement;
+}
 
+export const AppCreate: React.FunctionComponent<CreateProps> = ( props ): React.ReactElement => {
+    const [cloud, setCloud] = React.useState('gcp');
+    const regions = regionChoices(cloud);
+
+    return (
+        <Create {...props}>
+            <SimpleForm>
+                <TextInput source="name" label="App Name"/>
+                <SelectInput source="cloud" defaultValue='gcp' choices={[
+                    { id: 'gcp', name: 'Google Cloud Platform' },
+                    { id: 'aws', name: 'Amazon Web Services' },
+                ]} onChange={(_event: React.FormEvent, key: string): void => setCloud(key)}/>
+                {cloud !== "" &&
+                    <SelectInput source="region" {...regions}/>
+                }
+            </SimpleForm>
+        </Create>
+    );
+}
--- src/dataProvider.ts
+++ src/dataProvider.ts
@@ -1,29 +1,50 @@
 import logger from './logger';
 import {
-    // fetchUtils,
-    GET_LIST,
-    // GET_ONE,
-    // GET_MANY,
-    // GET_MANY_REFERENCE,
-    CREATE,
-    // UPDATE,
-    // UPDATE_MANY,
-    // DELETE,
-    // DELETE_MANY,
+// fetchUtils,
+// GET_LIST,
+// GET_ONE,
+// GET_MANY,
+// GET_MANY_REFERENCE,
+// CREATE,
+// UPDATE,
+// UPDATE_MANY,
+// DELETE,
+// DELETE_MANY,
 } from 'react-admin';
 import * as apps from './api/apps';
 
-export default (type: string, resource: string, params: any) => {
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface GetListParams {} 
+interface CreateParams {
+    data: {
+        name: string;
+        cloud: string;
+        region: string;
+    };
+}
+
+const isGetList = (params: CreateParams | GetListParams, type: string): params is GetListParams => type === 'GET_LIST'
+const isCreate = (params: CreateParams | GetListParams, type: string): params is CreateParams => type === 'CREATE'
+
+// can I use imported CREATE and GET_LIST instead?
+// const a = ['GET_LIST', 'CREATE'] as const
+// type DataProviderType = typeof a[number]
+type DataProviderType = 'GET_LIST' | 'CREATE'
+
+function foo<T extends DataProviderType>(type: T, resource: string, params: T extends 'CREATE' ? CreateParams : GetListParams): Promise<{}> {
     logger.debug(type, resource, params)
-    if (type === GET_LIST) {
+    if (isGetList(params, type)) {
         if (resource === 'apps') {
             return apps.get();
         }
-    } else if (type === CREATE) {
+    }
+    if (isCreate(params, type)) {
         if (resource === 'apps') {
             const { name, cloud, region } = params.data 
             return apps.create(name, cloud, region);
         }
     }
     throw new Error('not implemented yet');
-};
+}
+
+export default foo
