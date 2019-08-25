00f2f4ade11c4464d5da197a535a8f7ba73fa11f
Jesse <jesse@gigalixir.com>
2019-08-25 16:30:43 -0700

    remove custom routes now that we are just toggling the drawer manually with setState.

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,9 @@ import { Admin, Resource } from 'react-admin';
 import authProvider from './authProvider';
 import dataProvider from './dataProvider';
 import { AppList, AppCreate, AppShow } from './apps'
-import customRoutes from './customRoutes';
 
 const App = () => (
-    <Admin customRoutes={customRoutes} authProvider={authProvider} dataProvider={dataProvider}>
+    <Admin authProvider={authProvider} dataProvider={dataProvider}>
         <Resource name="apps" show={AppShow} list={AppList} create={AppCreate}/>
     </Admin>
 );
