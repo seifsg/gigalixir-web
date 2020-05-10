d8d894c9f736298f5ce32d0d9e8c1f703d94e856
Jesse <jesse@gigalixir.com>
2020-05-10 09:39:29 -0700

    format

--- src/AppBar.tsx
+++ src/AppBar.tsx
@@ -1,5 +1,4 @@
 import React, { Children, cloneElement } from 'react'
-// import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import classNames from 'classnames'
 import MuiAppBar from '@material-ui/core/AppBar'
