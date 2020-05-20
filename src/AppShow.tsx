9d4c2f688c389f43628d6f8bf0ae1bd7d88277a3
Jesse <jesse@gigalixir.com>
2020-05-20 13:58:56 -0700

    pull Charts out.

--- src/AppShow.tsx
+++ src/AppShow.tsx
@@ -1,19 +1,18 @@
+import React from 'react'
 import { withRouter } from 'react-router'
+import Charts from './Charts'
 import { ReduxState } from 'ra-core'
 import { push as routerPush } from 'react-router-redux'
 import compose from 'recompose/compose'
-import { withStyles, WithStyles, createStyles } from '@material-ui/core/styles'
-import React, { FunctionComponent } from 'react'
-import { Query, ShowController } from 'react-admin'
+import { withStyles } from '@material-ui/core/styles'
+import { ShowController } from 'react-admin'
 import { connect } from 'react-redux'
 import _ from 'lodash/fp'
 import { CorrectedReduxState } from './CorrectedReduxState'
 import AppScaleDialog from './AppScaleDialog'
-import { point, Stats } from './api/stats'
 import { StyledTab, StyledTabs } from './Tabs'
 import { App } from './api/apps'
 import { User } from './api/users'
-import Chart, { ChartPoint } from './Chart'
 import Page from './Page'
 import Section from './Section'
 import Fields from './Fields'
@@ -30,98 +29,6 @@ interface ShowProps {
   basePath: string
   resource: string
 }
-
-const chartStyles = createStyles({
-  loading: {
-    marginTop: 40
-  },
-  chartSection: {
-    display: 'flex',
-    // this is paired with the child's padding
-    // to produce space between elements
-    marginLeft: -20,
-    marginRight: -20
-  }
-})
-
-interface ChartsProps extends WithStyles<typeof chartStyles> {
-  id: string
-}
-interface Error {
-  message: string
-}
-const formatStatsPointForChart = (p: point): ChartPoint => {
-  const [x] = p
-  const [, y] = p
-  if (x === null) {
-    throw new Error('data point x can not be null')
-  }
-  return {
-    x,
-    y
-  }
-}
-const formatStatsForChart = (data: point[]): ChartPoint[] =>
-  data.map(formatStatsPointForChart)
-
-const toMegabytes = (data: ChartPoint[]): ChartPoint[] =>
-  data.map(
-    (p: ChartPoint): ChartPoint => {
-      let { x: time, y: value } = p
-      if (value !== null) {
-        value /= 1000000 // convert to megabytes
-      }
-      return { x: time, y: value }
-    }
-  )
-
-export const Charts: React.FunctionComponent<ChartsProps> = (
-  props
-): React.ReactElement => {
-  const { classes, id } = props
-  return (
-    <Query type="GET_ONE" resource="stats" payload={{ id }}>
-      {({
-        data,
-        loading,
-        error
-      }: {
-        data: Stats
-        loading: boolean
-        error: Error
-      }): React.ReactElement => {
-        // Note: loading here is not from the redux store so it is
-        // safe to use! The problem is Query behind the scenes uses
-        // dataProvider which will set the redux loading state even
-        // though we do not use it here.
-        if (loading) {
-          return (
-            <div className={classes.loading}>
-              <Loading />
-            </div>
-          )
-        }
-        if (error) {
-          return <div>Error: {error.message}</div>
-        }
-        return (
-          <div className={classes.chartSection}>
-            <Chart
-              data={formatStatsForChart(data.data.cpu)}
-              title="CPU (Millicores)"
-            />
-            <Chart
-              data={toMegabytes(formatStatsForChart(data.data.mem))}
-              title="Memory (MB)"
-            />
-          </div>
-        )
-      }}
-    </Query>
-  )
-}
-
-const EnhancedCharts = withStyles(chartStyles)(Charts)
 
 
 const Setup = (props: { profile: User; app: App }) => {
@@ -370,7 +277,7 @@ class AppShow extends React.Component<AppShowProps> {
                   </DialogButton>
                 </Fields>
               </Section>
-              <EnhancedCharts id={id} />
+              <Charts id={id} />
             </div>
           )
         }
---
+++ src/Charts.tsx
@@ -0,0 +1,100 @@
+import React, { FunctionComponent } from 'react'
+import { withStyles, WithStyles, createStyles } from '@material-ui/core/styles'
+import { Query} from 'react-admin'
+import Loading from './Loading'
+import Chart, { ChartPoint } from './Chart'
+import { point, Stats } from './api/stats'
+
+const chartStyles = createStyles({
+  loading: {
+    marginTop: 40
+  },
+  chartSection: {
+    display: 'flex',
+    // this is paired with the child's padding
+    // to produce space between elements
+    marginLeft: -20,
+    marginRight: -20
+  }
+})
+
+interface ChartsProps extends WithStyles<typeof chartStyles> {
+  id: string
+}
+interface Error {
+  message: string
+}
+const formatStatsPointForChart = (p: point): ChartPoint => {
+  const [x] = p
+  const [, y] = p
+  if (x === null) {
+    throw new Error('data point x can not be null')
+  }
+  return {
+    x,
+    y
+  }
+}
+const formatStatsForChart = (data: point[]): ChartPoint[] =>
+  data.map(formatStatsPointForChart)
+
+const toMegabytes = (data: ChartPoint[]): ChartPoint[] =>
+  data.map(
+    (p: ChartPoint): ChartPoint => {
+      let { x: time, y: value } = p
+      if (value !== null) {
+        value /= 1000000 // convert to megabytes
+      }
+      return { x: time, y: value }
+    }
+  )
+
+export const Charts: React.FunctionComponent<ChartsProps> = (
+  props
+): React.ReactElement => {
+  const { classes, id } = props
+  return (
+    <Query type="GET_ONE" resource="stats" payload={{ id }}>
+      {({
+        data,
+        loading,
+        error
+      }: {
+        data: Stats
+        loading: boolean
+        error: Error
+      }): React.ReactElement => {
+        // Note: loading here is not from the redux store so it is
+        // safe to use! The problem is Query behind the scenes uses
+        // dataProvider which will set the redux loading state even
+        // though we do not use it here.
+        if (loading) {
+          return (
+            <div className={classes.loading}>
+              <Loading />
+            </div>
+          )
+        }
+        if (error) {
+          return <div>Error: {error.message}</div>
+        }
+        return (
+          <div className={classes.chartSection}>
+            <Chart
+              data={formatStatsForChart(data.data.cpu)}
+              title="CPU (Millicores)"
+            />
+            <Chart
+              data={toMegabytes(formatStatsForChart(data.data.mem))}
+              title="Memory (MB)"
+            />
+          </div>
+        )
+      }}
+    </Query>
+  )
+}
+
+const EnhancedCharts = withStyles(chartStyles)(Charts)
+
+export default EnhancedCharts
