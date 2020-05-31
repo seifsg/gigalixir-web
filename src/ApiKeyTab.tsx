c8cec8afa4f9d528e2849534dd137823cdcb4b55
Jesse Shieh <jesseshieh@users.noreply.github.com>
2020-05-30 17:50:38 -0700

    Update ApiKeyTab.tsx

--- src/ApiKeyTab.tsx
+++ src/ApiKeyTab.tsx
@@ -1,11 +1,3 @@
-/*
-Author: Seif Sgayer (seif@sghaier.me)
-ApiKeyTab.tsx (c) 2020
-Desc: description
-Created:  2020-05-30T17:11:17.070Z
-Modified: 2020-05-30T21:53:22.042Z
-*/
-
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import { ReduxState } from 'ra-core'
